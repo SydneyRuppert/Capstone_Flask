628fa3a7afd776fa58e72a080dea22b780059775
Sydney Ruppert <sydneyruppert@outlook.com>
2023-12-30 11:17:50 -0800

    first commit

---
+++ app/api/routes.py
@@ -0,0 +1,76 @@
+from flask import Blueprint, request, jsonify, render_template
+from helpers import token_required
+from models import db, User, Contact, contact_schema, contacts_schema
+
+api = Blueprint('api',__name__, url_prefix='/api')
+
+
+#Creating
+@api.route('/plants', methods = ['POST'])
+@token_required
+def create_plant(current_user_token):
+
+    print(request.json)
+    name = request.json['name']
+    family = request.json['family']
+    genus = request.json['genus']
+    species = request.json['species']
+    common_name =request.json['common_name']
+    origin = request.json['origin']
+    user_token = current_user_token.token
+
+    print(f'BIG TESTER: {current_user_token.token}')
+
+
+
+
+    contact = Contact(name, family, genus, species, common_name, origin, user_token = user_token )
+    print(contact)
+    db.session.add(contact)
+    db.session.commit()
+
+    response = contact_schema.dump(contact)
+    return jsonify(response)
+#Retrieving all 
+@api.route('/plants/', methods=['GET'])
+@token_required
+def get_all_plants(current_user_token):
+    a_user=current_user_token.token
+    contacts=Contact.query.filter_by(user_token=a_user).all()
+    response=contacts_schema.dump(contacts)
+    return jsonify(response)
+
+
+#Retrieving single
+@api.route('plants/<id>',methods=['GET'])
+@token_required
+def get_single_plant(current_user_token,id):
+    contact=Contact.query.get(id)
+    response=contact_schema.dump(contact)
+    return jsonify(response)
+
+#Updating
+@api.route('/plants/<id>', methods=['POST','PUT'])
+@token_required
+def update_car(current_user_token, id):
+    contact=Contact.query.get(id)
+    contact.name = request.json['name']
+    contact.family = request.json['family']
+    contact.genus = request.json['genus']
+    contact.species = request.json['species']
+    contact.common_name =request.json['common_name']
+    contact.origin = request.json['origin']
+
+    db.session.commit()
+    response = contact_schema.dump(contact)
+    return jsonify(response)
+
+#Deleting car
+@api.route('/plants/<id>', methods=['DELETE'])
+@token_required
+def delete_car(current_user_token,id):
+    contact= Contact.query.get(id)
+    db.session.delete(contact)
+    db.session.commit()
+    response= contact_schema.dump(contact)
+    return jsonify(response)
