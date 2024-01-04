5adb2b2936f6edc04d650afc2d5e49852ea1e038
Sydney Ruppert <sydneyruppert@outlook.com>
2024-01-03 19:28:28 -0600

    updated commit

--- models.py
+++ models.py
@@ -73,7 +73,7 @@ class Contact(db.Model):
 
 
     def __repr__(self):
-        return f'The following contact has been added to the phonebook: {self.name}'
+        return f'The following contact has been added to the plant inventory: {self.name}'
 
     def set_id(self):
         return (secrets.token_urlsafe())
